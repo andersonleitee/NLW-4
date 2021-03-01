3b47f75dc67f009ec65d5bd8348a064e8bb03b16
Anderson Leite <andersondias@cc.ci.ufpb.br>
2021-02-28 21:11:02 -0300

    Upgrade aula 1

--- moveit/src/App.tsx
+++ moveit/src/App.tsx
@@ -1,10 +1,12 @@
 
 import './styles/global.css';
+import { ExperienceBar } from './components/ExperienceBar'
+
 
 function App() {
   return (
-      <div>
-       
+      <div className = "container"> 
+       <ExperienceBar />
        </div>
 
   );
